3a2b599ee3d0a56f188fef9bef646da367d1d855
syng <syngyang@gmail.com>
2022-06-30 13:32:58 +0900

    formik_with_initialValues

---
+++ src/components/YoutubeForm.js
@@ -0,0 +1,35 @@
+import { useFormik } from 'formik'
+import React from 'react'
+
+const YoutubeForm = () => {
+    const formik = useFormik({
+        initialValues: {
+            name:'syng',
+            email:'',
+            channel:''
+        },
+        onSubmit: values =>{
+            console.log('Form data', values)
+        }
+
+    })
+// console.log('Form values', formik.values)
+  return (
+    <div>
+        <form onSubmit={formik.handleSubmit}>
+            <label htmlFor="name">Name</label>
+            <input type="text" id='name' name='name' onChange={formik.handleChange} value={formik.values.name}/>
+
+            <label htmlFor="email">Email</label>
+            <input type="email" id='email' name='email' onChange={formik.handleChange} value={formik.values.email}/>
+
+            <label htmlFor="channel" id='channel' name='channel'>Channel</label>
+            <input type="text" id='channel' name='channel' onChange={formik.handleChange} value={formik.values.channel}/>
+
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+  )
+}
+
+export default YoutubeForm
